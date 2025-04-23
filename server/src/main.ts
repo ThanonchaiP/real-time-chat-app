23176530bb02eff5028662fcdf6a5177cd99ec42
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-04-23 20:57:34 +0700

    refactor: update Prettier config, add new dependencies, and modify app service response

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
