d7bde2e5b6de19ba2789718b43059a7235f7e939
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-04-24 21:02:44 +0700

    feat: add MongoDB configuration and update CORS settings in the application

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://admin:1234@localhost:27017', {
+      dbName: 'realtime_chat',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000', // frontend URL
+    credentials: true, // ✅ ต้องเปิดเพื่อรับ cookie
+  });
   app.use(helmet());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
