4594e2b1f6635fe42269c2d8922f27d27fe6fad4
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-04-28 20:50:23 +0700

    feat: implement rooms module with controller, service, and DTOs for room management

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ﻿# Realtime Chat App (Under Development)
 
 A real-time chat application built with Next.js, NestJS, Socket.IO, MongoDB, and Redis.
+
+nest g resource cats --type rest --no-spec
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
     ),
     UsersModule,
     AuthModule,
+    RoomsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
