19db9e28ec22ba6e3f39e30f7f57c71a53b10359
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-04-29 21:29:37 +0700

    feat: define Room entity with properties and schema for room management

--- server/src/rooms/entities/room.entity.ts
+++ server/src/rooms/entities/room.entity.ts
@@ -1,1 +1,21 @@
-export class Room {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type CatDocument = HydratedDocument<Room>;
+
+@Schema({ timestamps: true })
+export class Room {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  type: 'direct' | 'group';
+
+  @Prop({ required: true })
+  participants: Array<string>;
+
+  @Prop({ required: true })
+  createdBy: string;
+}
+
+export const RoomSchema = SchemaFactory.createForClass(Room);
