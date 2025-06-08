4286affb95ec2100befce147a7e4cfee9770f7ce
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-06-08 15:39:04 +0700

    feat: add login and registration pages with form validation and user feedback

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -4,16 +4,18 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("access_token")?.value;
 
+  // ถ้ามี token และกำลังเข้าถึงหน้า login หรือ register, redirect ไปหน้า home
+  if (
+    token &&
+    (req.nextUrl.pathname.startsWith("/login") ||
+      req.nextUrl.pathname.startsWith("/register"))
+  ) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
   // ถ้าไม่มี token, redirect ไป login
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  if (
-    req.nextUrl.pathname.startsWith("/login") ||
-    req.nextUrl.pathname.startsWith("/register")
-  ) {
-    return NextResponse.redirect(new URL("/", req.url));
   }
 
   // ถ้ามี token ให้ผ่านได้
