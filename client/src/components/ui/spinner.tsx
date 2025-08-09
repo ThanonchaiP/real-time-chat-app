fc92c07ddad34dc260a1db19b474b8925e47df42
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-08-09 20:12:09 +0700

    feat: add LoadingSpinner component with customizable size and className

--- client/src/components/ui/spinner.tsx
+++ client/src/components/ui/spinner.tsx
@@ -1,7 +1,38 @@
+import { cn } from "@/lib/utils";
+
 export const Spinner = () => {
   return (
     <div className="flex items-center justify-center h-full">
       <div className="animate-spin rounded-full h-8 w-8 border-t-4 border-blue-500"></div>
     </div>
+  );
+};
+
+interface ISVGProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  className?: string;
+}
+
+export const LoadingSpinner = ({
+  size = 24,
+  className,
+  ...props
+}: ISVGProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("animate-spin", className)}
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
   );
 };
