5a82019fb9cbc4da87fdf23aa2fade79ddd807d9
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-09-27 16:10:01 +0700

    feat: add Dockerfile and .dockerignore for client, update next.config for standalone output

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     domains: ["chatapp-media-assets.s3.ap-southeast-7.amazonaws.com"],
   },
